cc30596ab47b01077fbb6d5f0379fad99d90ac7f
RumbleKAT <reki318@naver.com>
2022-03-23 23:58:20 +0900

    [feat] loading bar #1

---
+++ src/components/Loadingbar.vue
@@ -0,0 +1,25 @@
+<template>
+<div :style="{ display : active } ">
+    Loading Bar
+</div>
+</template>
+
+<script>
+export default {
+    name : 'LoadingBar',
+    props : ["toggled"],
+    computed : { 
+        active(){
+            if(this.toggled.active){
+                return "block"
+            }else{
+                return "none";
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
